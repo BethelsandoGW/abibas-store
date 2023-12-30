45593766c27eb5051c86ee509de5d55710c86523
itsmvx <xenovzx@gmail.com>
2023-12-31 00:33:01 +0700

    Admin update components and layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,7 +2,7 @@ import AdminContentWrapper from "@/app/components/AdminContentWrapper";
 import { ReactNode } from "react";
 import dynamic from "next/dynamic";
 import { Metadata } from "next";
-const AdminSidebar = dynamic(() => import('@/app/components/AdminSideNav'), { ssr: false });
+const AdminSideNav = dynamic(() => import('@/app/components/AdminSideNav'), { ssr: false });
 const UserPrefsSettings = dynamic(() => import('@/app/components/UserPrefSettings'), { ssr: false });
 export const metadata: Metadata = {
     title: 'Abibas | Admin'
@@ -11,7 +11,7 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
     return (
         <>
             <main className="w-full min-h-screen flex flex-row font-tommy bg-gray-100 scroll-smooth">
-                <AdminSidebar />
+                <AdminSideNav />
                 <AdminContentWrapper>
                     { children }
                 </AdminContentWrapper>
